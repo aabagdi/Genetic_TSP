fa5bb3ff4c0e5852fbf05b80240353475223a4d6
deeptronos <ecnemec1132@gmail.com>
2021-05-09 22:07:36 -0500

    Commented tournament_deme.hh
    Added details about assignment from Moodle Page

--- tournament_deme.hh
+++ tournament_deme.hh
@@ -1,5 +1,15 @@
 #pragma once
 #include "deme.hh"
+//Assignment:
+// This class inherits from the Deme class and overrides whatever is necessary in your judgement for
+// your class to replace the parent selection algorithm.
+// It can use either Chromosome or ClimbChromosome: experiment with both when you're done and pick the best one.
+
+//"Tournament Selection" method:
+// A. Select P parents at random, where P is a constant power of two that you choose (no bigger than the population size).
+// B. Take the first pair in the set of P parents and discard the parent with the lower fitness of the two.
+// C. Do the same for the next pair, and so forth. You should have P/2 parents in the resulting set.
+// D. Repeat steps B - C until you're left with only one parent.
 
 class TournamentDeme : public deme{
 
