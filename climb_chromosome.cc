c511f17d5f8c88758d08cd84a373b862543ce14d
deeptronos <ecnemec1132@gmail.com>
2021-05-12 17:39:21 -0500

    Fixed memory leak; organized ClimbChromosome
    Added deletion to where ClimbChromo::clone() is used to solve memory leaks, and moved ClimbChromo::mutate() to the implementation (.cc) file from the definition (.hh) file

--- climb_chromosome.cc
+++ climb_chromosome.cc
@@ -11,6 +11,82 @@ ClimbChromosome::ClimbChromosome(const Cities * cities_ptr) : Chromosome(cities_
 	assert(is_valid());
 }
 
+ClimbChromosome::~ClimbChromosome() {
+	assert(is_valid());
+}
+
+void ClimbChromosome::mutate() {
+
+	//Evaluate fitness of original chromosome
+	auto original_fitness = get_fitness();
+
+	std::uniform_int_distribution<int> distr(0, order_.size() - 1);
+
+	//Pick a random point p in the chromosme
+	auto p = distr(generator_);
+
+	//Clone original chromosome for swap
+	auto left_swapped = this->clone();
+
+	if(p == 0){
+		//If random index is 0, swap it with the last element of order_
+
+		std::swap(left_swapped->order_[0], left_swapped->order_[ (left_swapped->order_.size() - 1) ]);
+	}else {
+		//Else swap element at p with element at p - 1
+
+		std::swap(left_swapped->order_[p], left_swapped->order_[ (p-1) ]);
+
+	}
+	auto left_fitness = left_swapped->get_fitness();
+
+	//Clone original chromosome for second swap
+	auto right_swapped = this->clone();
+
+	if(static_cast<unsigned long>(p) == right_swapped->get_ordering().size() - 1){ //get_ordering.size() returns an unsigned long, so we must cast p, an int, to be comparable with it
+		//If random index is last element, swap it with the first element
+
+		std::swap(right_swapped->order_[0], right_swapped->order_[(right_swapped->order_.size() - 1)]);
+	}else {
+		//Else swap element p with element p + 1
+
+		std::swap(right_swapped->order_[p], right_swapped->order_[ (p+1) ]);
+	}
+
+	auto right_fitness = right_swapped->get_fitness();
+
+	//Determine the fittest of the 3 possibilities
+	double max = original_fitness;
+	if(left_fitness > max){
+		max = left_fitness;
+	}
+	if(right_fitness > max){
+		max = right_fitness;
+	}
+
+	if(max == original_fitness){
+		//order_ is unchanged
+		delete right_swapped;
+		delete left_swapped;
+		return;
+	}else if(max == right_fitness){
+		//right_swapped is fittest
+		order_ = right_swapped->get_ordering();     //Might be issue with get ordering returning an immutable reference?
+		delete right_swapped;
+		delete left_swapped;
+		return;
+	}else if(max == left_fitness){
+		//left_swapped is fittest
+		order_ = left_swapped->get_ordering();
+		delete right_swapped;
+		delete left_swapped;
+		return;
+	}
+	delete right_swapped;
+	delete left_swapped;
+	return;
+}
+
 //The new mutation operator we’ll try is called “local hill climbing”, because it searches for the best solution in
 // a small local “neighborhood” of the current chromosome. It works as follows (assuming N cities, i.e. a chromosome with N genes):
 	// A. evaluate the fitness of the current (original) chromosome.
--- climb_chromosome.hh
+++ climb_chromosome.hh
@@ -11,11 +11,12 @@ public:
 ClimbChromosome(const Cities*);
 
 //Always explicitly write a virtual destructor
-virtual ~ClimbChromosome() = default;
+virtual ~ClimbChromosome();
 
 //Override clone
-virtual ClimbChromosome* clone() const override {
-	return new ClimbChromosome(cities_ptr_);
+ClimbChromosome* clone() const override {
+	auto chromo = new ClimbChromosome(cities_ptr_);
+	return chromo;
 }
 
 //virtual void mutate() override;
@@ -29,69 +30,7 @@ virtual ClimbChromosome* clone() const override {
 	// E. Find which of the three chromosomes (i.e., chromosomes from steps A, C, and D) produced the fittest ordering and choose that fittest chromosome as the resulting mutated chromosome.
 
 //Overide mutate
-virtual void mutate() override
-{
-    //Evaluate fitness of original chromosome
-    auto original_fitness = get_fitness();
-
-    std::uniform_int_distribution<int> distr(0, order_.size() - 1);
-
-    //Pick a random point p in the chromosme
-    auto p = distr(generator_);
-
-    //Clone original chromosome for swap
-    auto left_swapped = clone();
-
-    if(p == 0){
-        //If random index is 0, swap it with the last element of order_
-        //std::iter_swap(left_swapped->order_.begin(), left_swapped->order_.end());
-        std::swap(left_swapped->order_[0], left_swapped->order_[ (left_swapped->order_.size() - 1) ]);
-    }else {
-	    //Else swap element at p with element at p - 1
-	    //std::iter_swap(left_swapped->order_.begin() + p, left_swapped->order_.begin() + (p - 1));
-	    std::swap(left_swapped->order_[p], left_swapped->order_[ (p-1) ]);
-
-    }
-    auto left_fitness = left_swapped->get_fitness();
-
-    //Clone original chromosome for second swap
-    auto right_swapped = clone();
-
-    if(static_cast<unsigned long>(p) == right_swapped->get_ordering().size() - 1){ //get_ordering.size() returns an unsigned long, so we must cast p, an int, to be comparable with it
-        //If random index is last element, swap it with the first element
-        //std::iter_swap(right_swapped->order_.begin(), right_swapped->order_.end());
-        std::swap(right_swapped->order_[0], right_swapped->order_[(right_swapped->order_.size() - 1)]);
-    }else {
-	    //Else swap element p with element p + 1
-	    //std::iter_swap(right_swapped->order_.begin() + p, right_swapped->order_.begin() + (p + 1));
-	    std::swap(right_swapped->order_[p], right_swapped->order_[ (p+1) ]);
-    }
-
-    auto right_fitness = right_swapped->get_fitness();
-
-    //Determine the fittest of the 3 possibilities
-    double max = original_fitness;
-    if(left_fitness > max){
-        max = left_fitness;
-    }
-    if(right_fitness > max){
-        max = right_fitness;
-    }
-
-    if(max == original_fitness){
-        //order_ is unchanged
-        return;
-    }else if(max == right_fitness){
-        //right_swapped is fittest
-        order_ = right_swapped->get_ordering();     //Might be issue with get ordering returning an immutable reference?
-        return;
-    }else if(max == left_fitness){
-        //left_swapped is fittest
-        order_ = left_swapped->get_ordering();
-        return;
-    }
-    //return;
-}
+virtual void mutate() override;
 
 
 
